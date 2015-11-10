ddabdf90c8d7945206b60a9267164549c033daca
pxhanus <pxhanus@gmail.com>
2015-11-10 14:21:43 -0600

    Initial commit

---
+++ LetterGrade/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPrompt = New System.Windows.Forms.Label()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.btnLetterGrade = New System.Windows.Forms.Button()
+        Me.lblLetterGrade = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblPrompt
+        '
+        Me.lblPrompt.AutoSize = True
+        Me.lblPrompt.Location = New System.Drawing.Point(15, 26)
+        Me.lblPrompt.Name = "lblPrompt"
+        Me.lblPrompt.Size = New System.Drawing.Size(93, 13)
+        Me.lblPrompt.TabIndex = 0
+        Me.lblPrompt.Text = "Enter a test score:"
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(114, 23)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(57, 20)
+        Me.txtScore.TabIndex = 1
+        '
+        'btnLetterGrade
+        '
+        Me.btnLetterGrade.Location = New System.Drawing.Point(177, 17)
+        Me.btnLetterGrade.Name = "btnLetterGrade"
+        Me.btnLetterGrade.Size = New System.Drawing.Size(74, 31)
+        Me.btnLetterGrade.TabIndex = 2
+        Me.btnLetterGrade.Text = "Letter Grade"
+        Me.btnLetterGrade.UseVisualStyleBackColor = True
+        '
+        'lblLetterGrade
+        '
+        Me.lblLetterGrade.AutoSize = True
+        Me.lblLetterGrade.Location = New System.Drawing.Point(12, 64)
+        Me.lblLetterGrade.Name = "lblLetterGrade"
+        Me.lblLetterGrade.Size = New System.Drawing.Size(0, 13)
+        Me.lblLetterGrade.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(267, 93)
+        Me.Controls.Add(Me.lblLetterGrade)
+        Me.Controls.Add(Me.btnLetterGrade)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.lblPrompt)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPrompt As System.Windows.Forms.Label
+    Friend WithEvents txtScore As System.Windows.Forms.TextBox
+    Friend WithEvents btnLetterGrade As System.Windows.Forms.Button
+    Friend WithEvents lblLetterGrade As System.Windows.Forms.Label
+
+End Class
---
+++ LetterGrade/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Private Sub btnLetterGrade_Click(sender As Object, e As EventArgs) Handles btnLetterGrade.Click
+        Const LOWEST_SCORE As Double = 0
+        Const HIGHEST_SCORE As Double = 100
+        Dim scoreEntered As Double
+
+        scoreEntered = Val(Me.txtScore.Text)
+
+        If Not ValidEntry(scoreEntered, HIGHEST_SCORE, LOWEST_SCORE) Then
+            MessageBox.Show("Enter a score between " & LOWEST_SCORE & " and " & HIGHEST_SCORE)
+            Me.txtScore.Text = Nothing
+            Me.lblLetterGrade.Text = Nothing
+        Else
+            Me.lblLetterGrade.Text = "Your grade is a(n) " & LetterGrade(scoreEntered)
+        End If
+    End Sub
+    '********************************************************************************************
+    'ValidEntry
+    'Returns True if lowerLimit <= userNum <= upperLimit.
+    '
+    'post: True has been returned if lowerLimit <= userNum <= upperLimit.
+    'False has been returned otherwise.
+    '********************************************************************************************
+    Function ValidEntry(ByVal userNum As Integer, ByVal upperLimit As Integer, _
+                         ByVal lowerLimit As Integer) As Boolean
+        If userNum > upperLimit Or userNum < lowerLimit Then
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+    '********************************************************************************************
+    'LetterGrade
+    'Returns a letter grade corresponding to score.
+    '
+    'post: A letter grade (A, B, C, D, or F) has been returned.
+    '********************************************************************************************
+    Function LetterGrade(ByVal score As Double) As Char
+        If score >= 90 Then
+            Return "A"
+        ElseIf score >= 80 Then
+            Return "B"
+        ElseIf score >= 70 Then
+            Return "C"
+        ElseIf score >= 60 Then
+            Return "D"
+        Else
+            Return "F"
+        End If
+    End Function
+
+    Private Sub txtScore_TextChanged(sender As Object, e As EventArgs) Handles txtScore.TextChanged
+        Me.lblLetterGrade.Text = Nothing
+    End Sub
+End Class
